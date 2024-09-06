0bc2eee07ebb924753da926ddc1736d2f152a842
Sascha Metz <hello@smetz.dev>
2024-09-06 16:03:17 +0200

    chore: :heavy_plus_sign: add @formkit/auto-animate

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
-createApp(App).mount('#app')
+createApp(App).use(autoAnimatePlugin).mount('#app')
