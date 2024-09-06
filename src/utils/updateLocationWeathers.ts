78dd0348f27f03a76f851d63af76d85d7d18a88a
Sascha Metz <hello@smetz.dev>
2024-09-06 17:25:18 +0200

    refactor(utils/updateLocationsWeathers): :safety_vest: catch errors

--- src/utils/updateLocationWeathers.ts
+++ src/utils/updateLocationWeathers.ts
@@ -4,11 +4,18 @@ import { fetchLocationWeather } from './fetchLocationWeather'
 export const updateLocationWeathers = async (oldLocations: LocationWithWeather[]) => {
   const updatedLocations = await Promise.all(
     oldLocations.map(async (location) => {
-      const updatedTemperature = await fetchLocationWeather(location.lat, location.lon)
-      return {
-        ...location,
-        temperature: updatedTemperature
+      try {
+        const updatedTemperature = await fetchLocationWeather(location.lat, location.lon)
+        return {
+          ...location,
+          temperature: updatedTemperature
+        }
+      } catch (_) {
+        console.log(`Could not update ${location.label}`)
       }
+
+      // If update fails, just return the old location...
+      return location
     })
   )
 
