4f320c6e6312b2502c7502bc037baa524fd4c52b
Sascha Metz <hello@smetz.dev>
2024-09-06 16:02:29 +0200

    refactor(components/LocationList): :fire: remove defineProps import

--- src/components/LocationList.vue
+++ src/components/LocationList.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { defineProps } from 'vue'
 import LocationListItem from './LocationListItem.vue'
 import { type LocationWithWeather } from '@/types/LocationWithWeather'
 
