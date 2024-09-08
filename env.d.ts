68e4e29cfb3f9d11eeb88b5f8a8ff4be97cf4487
Sascha Metz <hello@smetz.dev>
2024-09-08 15:51:42 +0200

    refactor(env): :building_construction: use env.d.ts instead of env.example

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_WEATHER_API_KEY: string
+  readonly VITE_MAPBOX_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
